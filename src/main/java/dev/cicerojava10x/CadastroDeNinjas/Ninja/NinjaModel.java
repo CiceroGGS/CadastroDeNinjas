52776b41683d1fd5cd41f882dd0766b81efed55d
CiceroGGS <cicerog.silvestre@gmail.com>
2025-04-02 13:23:40 -0300

    feat: Adicionado @Columns para declarar o nome dos atributos nas tabelas

--- src/main/java/dev/cicerojava10x/CadastroDeNinjas/Ninja/NinjaModel.java
+++ src/main/java/dev/cicerojava10x/CadastroDeNinjas/Ninja/NinjaModel.java
@@ -17,14 +17,20 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
 
+    @Column(name = "nome")
     private String nome;
 
-    @Column(unique = true)
+    @Column(unique = true, name = "email")
     private String email;
 
+    @Column(name = "idade")
     private int idade;
+
+    @Column(name = "img_url")
+    private String imgUrl;
 
     @ManyToOne
     @JoinColumn(name = "missoes_id")
