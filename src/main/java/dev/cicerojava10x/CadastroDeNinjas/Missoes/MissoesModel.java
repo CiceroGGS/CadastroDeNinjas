c726787f1679e439db10eafe32e9a44ea0c6dba8
CiceroGGS <cicerog.silvestre@gmail.com>
2025-04-02 12:54:37 -0300

    feat: Adicionado lombok no projeto

--- src/main/java/dev/cicerojava10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/cicerojava10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,12 +2,22 @@ package dev.cicerojava10x.CadastroDeNinjas.Missoes;
 
 import dev.cicerojava10x.CadastroDeNinjas.Ninja.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@AllArgsConstructor
+@Data
+@Getter
+@Setter
 public class MissoesModel {
+
+    /******************************************/
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,54 +30,56 @@ public class MissoesModel {
 
     /******************************************/
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(long id, String nomeDaMissao, String dificuldade) {
-        this.id = id;
-        this.nomeDaMissao = nomeDaMissao;
-        this.dificuldade = dificuldade;
-    }
-
-    public MissoesModel(long id, String nomeDaMissao, String dificuldade, List<NinjaModel> ninjas) {
-        this.id = id;
-        this.nomeDaMissao = nomeDaMissao;
-        this.dificuldade = dificuldade;
-        this.ninjas = ninjas;
-    }
+//    public MissoesModel() {
+//    }
+//
+//    public MissoesModel(long id, String nomeDaMissao, String dificuldade) {
+//        this.id = id;
+//        this.nomeDaMissao = nomeDaMissao;
+//        this.dificuldade = dificuldade;
+//    }
+//
+//    public MissoesModel(long id, String nomeDaMissao, String dificuldade, List<NinjaModel> ninjas) {
+//        this.id = id;
+//        this.nomeDaMissao = nomeDaMissao;
+//        this.dificuldade = dificuldade;
+//        this.ninjas = ninjas;
+//    }
 
     /******************************************/
 
-    public long getId() {
-        return id;
-    }
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public String getNomeDaMissao() {
+//        return nomeDaMissao;
+//    }
+//
+//    public void setNomeDaMissao(String nomeDaMissao) {
+//        this.nomeDaMissao = nomeDaMissao;
+//    }
+//
+//    public String getDificuldade() {
+//        return dificuldade;
+//    }
+//
+//    public void setDificuldade(String dificuldade) {
+//        this.dificuldade = dificuldade;
+//    }
+//
+//    public List<NinjaModel> getNinja() {
+//        return ninjas;
+//    }
+//
+//    public void setNinja(List<NinjaModel> ninja) {
+//        this.ninjas = ninja;
+//    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNomeDaMissao() {
-        return nomeDaMissao;
-    }
-
-    public void setNomeDaMissao(String nomeDaMissao) {
-        this.nomeDaMissao = nomeDaMissao;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public List<NinjaModel> getNinja() {
-        return ninjas;
-    }
-
-    public void setNinja(List<NinjaModel> ninja) {
-        this.ninjas = ninja;
-    }
+    /******************************************/
 
 }
--- src/main/java/dev/cicerojava10x/CadastroDeNinjas/Ninja/NinjaModel.java
+++ src/main/java/dev/cicerojava10x/CadastroDeNinjas/Ninja/NinjaModel.java
@@ -2,12 +2,18 @@ package dev.cicerojava10x.CadastroDeNinjas.Ninja;
 
 import dev.cicerojava10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_ninja")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Getter
+@Setter
 public class NinjaModel {
+
+    /******************************************/
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,54 +28,55 @@ public class NinjaModel {
 
     /******************************************/
 
-    public NinjaModel() {}
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public NinjaModel(long id, String nome, String email, int idade, MissoesModel missoes) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-        this.missoes = missoes;
-    }
+//    public NinjaModel() {}
+//
+//    public NinjaModel(String nome, String email, int idade) {
+//        this.nome = nome;
+//        this.email = email;
+//        this.idade = idade;
+//    }
+//
+//    public NinjaModel(long id, String nome, String email, int idade, MissoesModel missoes) {
+//        this.id = id;
+//        this.nome = nome;
+//        this.email = email;
+//        this.idade = idade;
+//        this.missoes = missoes;
+//    }
 
     /******************************************/
-    
-    public String getNome() {
-        return nome;
-    }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+//    public String getNome() {
+//        return nome;
+//    }
+//
+//    public void setNome(String nome) {
+//        this.nome = nome;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public int getIdade() {
+//        return idade;
+//    }
+//
+//    public void setIdade(int idade) {
+//        this.idade = idade;
+//    }
+//
+//    public MissoesModel getMissoes() {
+//        return missoes;
+//    }
+//
+//    public void setMissoes(MissoesModel missoes) {
+//        this.missoes = missoes;
+//    }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public MissoesModel getMissoes() {
-        return missoes;
-    }
-
-    public void setMissoes(MissoesModel missoes) {
-        this.missoes = missoes;
-    }
-
+    /******************************************/
 }
