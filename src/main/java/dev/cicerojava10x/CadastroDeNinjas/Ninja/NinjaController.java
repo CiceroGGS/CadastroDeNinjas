3c32ae3017b8c5492773780882aa1f0aa5b431ec
CiceroGGS <cicerog.silvestre@gmail.com>
2025-04-02 11:49:18 -0300

    feat: Criado NinjaController

---
+++ src/main/java/dev/cicerojava10x/CadastroDeNinjas/Ninja/NinjaController.java
@@ -0,0 +1,18 @@
+package dev.cicerojava10x.CadastroDeNinjas.Ninja;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Boas vindas";
+    }
+
+
+}
