e30360e02b5511587e9e44824c0c9025c9f99911
CiceroGGS <cicerog.silvestre@gmail.com>
2025-04-07 18:06:29 -0300

    feat: Criado NinjaControllerUI para renderizar para o usuario o JSON

--- src/main/java/dev/cicerojava10x/CadastroDeNinjas/Ninja/NinjaControllerUi.java
+++ src/main/java/dev/cicerojava10x/CadastroDeNinjas/Ninja/NinjaControllerUi.java
@@ -1,7 +1,11 @@
 package dev.cicerojava10x.CadastroDeNinjas.Ninja;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/ninjas/ui")
@@ -11,6 +15,14 @@ public class NinjaControllerUi {
 
     private NinjaControllerUi(NinjaService ninjaService){
         this.ninjaService = ninjaService;
+    }
+
+    @GetMapping("/listar")
+    public String listarNinjas(Model) {
+
+        List<NinjaDTO> ninjas =  ninjaService.listarNinjas();
+        return "Ninjas Listados";
+
     }
 
 }
