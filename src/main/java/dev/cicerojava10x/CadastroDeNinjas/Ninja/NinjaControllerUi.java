5d76ee3ad23acba091119cb3ff321bb5ccc3586d
CiceroGGS <cicerog.silvestre@gmail.com>
2025-04-07 22:56:18 -0300

    feat: Criada css e html para detalhar ninja e adiconar ninja

--- src/main/java/dev/cicerojava10x/CadastroDeNinjas/Ninja/NinjaControllerUi.java
+++ src/main/java/dev/cicerojava10x/CadastroDeNinjas/Ninja/NinjaControllerUi.java
@@ -4,10 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -26,7 +24,7 @@ public class NinjaControllerUi {
 
         List<NinjaDTO> ninjas =  ninjaService.listarNinjas();
         model.addAttribute("ninjas", ninjas);
-        return "listarNinjas";
+        return "listar_ninjas";
 
     }
 
@@ -36,6 +34,26 @@ public class NinjaControllerUi {
         ninjaService.deletarNinjaPorId(id);
         return "redirect:/ninjas/ui/listar";
 
+    }
+
+    @GetMapping("/listar/{id}")
+    public String detalhesNinja(@PathVariable Long id, Model model) {
+        NinjaDTO ninja = ninjaService.listarNinjasPorId(id);
+        model.addAttribute("ninja", ninja);
+        return "detalhes_ninja";
+    }
+
+    @GetMapping("/adicionar")
+    public String mostrarFormularioAdicionarNinja(Model model) {
+        model.addAttribute("ninja", new NinjaDTO());
+        return "adicionar_ninja";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarNinja(@ModelAttribute NinjaDTO ninja, RedirectAttributes redirectAttributes) {
+        ninjaService.criarNinja(ninja);
+        redirectAttributes.addFlashAttribute("mensagem", "Ninja cadastrado com sucesso!");
+        return "redirect:/ninjas/ui/listar";
     }
 
 }
